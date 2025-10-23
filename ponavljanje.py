e50ed889242dda9599277cacf7014d649aa1f65c
SebastianBenzin <sebabenvin@gmail.com>
2025-10-23 12:49:09 +0200

    Add files via upload

---
+++ ponavljanje.py
@@ -0,0 +1,53 @@
+'''
+Ovo je višeredni komentar
+'''
+
+# Ovo je jednolinijski komentar
+
+# 1. Tipovi podataka
+
+broj = 10 # (integer) - Cijeli broj
+tekst = "Ovo je rečenica." # string (tekst) - Niz znakova
+znak = 'a' # char (character) - znak
+
+# -*- coding: utf-8 -*-
+
+cijena = 1.5 #float - Decimalni broj
+istina = True # bool (boolean) - True/False
+
+
+
+# Grananje (if/elif/else)
+'''
+if broj > 5:
+    print("Broj je veći od 5.")
+elif broj == 5:
+    print("Broj je jednak 5.")
+else:
+    print("Broj je manji od 5.")
+
+if not istina:
+    print("False")
+else:
+    print("True")
+
+temperatura = int(input("Unesi trenutnu temperaturu: "))
+
+if 0 < temperatura <= 0 :
+    print("Ledenica")
+elif 0 <= temperatura <= 15:
+    print("Hladno")
+elif 15 < temperatura <= 25:
+    print("Ugodno")
+else:
+    print("Vruće")
+    
+'''
+
+# PETLJE
+
+for i in range(2, 23):
+    if i%2 == 0:
+        print (f"{i} je paran broj.")
+    else:
+        print (f"{i} je neparan broj.")
